96717ff892ff9baafffbe55c117fa073a1039e25
Charles  Mambo <charles.mambo@tauspace.com>
2026-02-04 01:09:32 +0200

    complete about stage 1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
-import '../footer/Footer.css';
-import LOGO from '../../assets/logo.svg'
+import React from "react";
+import "../footer/Footer.css";
+import LOGO from "../../assets/logo.svg";
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer>
-        <img src={LOGO} alt="web creatives logo" className="footer-logo" />
+      <img src={LOGO} alt="web creatives logo" className="footer-logo" />
 
-        <div className="footer-links">
-            <ul>
-                <li>
-                    <a href="">Contact Us</a>
-                </li>
-                <li>
-                    <a href="">About</a>
-                </li>
-                <li>
-                    <a href="">Privacy & Cookie Policy</a>
-                </li>
-                <li>
-                    <a href="">Terms & Conditions</a>
-                </li>
-            </ul>
-            <p>@{currentYear} Webb Creatives Agency.All rights reserved</p>
-        </div>
+      <div className="footer-links">
+        <ul>
+          <li>
+            <a href="">Contact Us</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Privacy & Cookie Policy</a>
+          </li>
+          <li>
+            <a href="">Terms & Conditions</a>
+          </li>
+        </ul>
+        <p className="year">
+          @{currentYear} Webb Creatives Agency.All rights reserved
+        </p>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
